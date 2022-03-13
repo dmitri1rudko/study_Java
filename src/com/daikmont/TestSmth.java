e9c836f3e35ea1ec8248708695de0460ba7028d4
dmitri1rudko <dmitriirudko@icloud.com>
2022-03-13 14:44:53 +0300

    1

---
+++ src/com/daikmont/TestSmth.java
@@ -0,0 +1,15 @@
+package com.daikmont;
+
+class Maths {
+    public int maxFinder(int a, int b) {
+        a = a + b;
+        return a;
+    }
+}
+
+public class TestSmth {
+    public static void main(String[] args) {
+        Maths maths = new Maths();
+        System.out.println(maths.maxFinder(2,5));
+    }
+}

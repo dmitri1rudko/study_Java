16f7cdff721632bb894ca91e82b6508850a63b43
dmitri1 <dmitriirudko@icloud.com>
2022-03-16 00:09:53 +0300

    none

---
+++ src/com/daikmont/study/Class1.java
@@ -0,0 +1,11 @@
+package com.daikmont.study;
+
+public class Class1 {
+    public static void main(String[] args) {
+        int number5= 5;
+        for (int i=1; i<11; i++){
+            System.out.println("5 * " + i + " = " + (number5*i));
+        }
+
+    }
+}

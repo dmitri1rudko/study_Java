729124742e87820d99282979ceff371289c9fe2a
dmitri1 <dmitriirudko@icloud.com>
2022-03-20 23:55:54 +0300

    new day

---
+++ src/com/daikmont/study/class1/DZ2.java
@@ -0,0 +1,12 @@
+package com.daikmont.study.class1;
+
+//        напишите метод который найдет сумму чисел.
+//        Между от 11 до 100 которые делятся на 4 без остатка
+
+public class DZ2 {
+
+    public static void main(String[] args) {
+
+    }
+
+}
